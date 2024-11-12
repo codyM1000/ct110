363c3fea391fb67019ae1dcb92155d2248a802e1
codyM1000 <codymatheney@yahoo.com>
2024-11-12 16:33:42 +0000

    added value returning functions

--- fuction_EX.py
+++ fuction_EX.py
@@ -5,8 +5,16 @@
 def my_animal(name, sound, pound_food):
     print(f"the {name} makes a {sound} noise!")
     print(f"the{name} eats {pound_food} pounds of food a day")
-    print(f"the{name} eats {pound_food * 7} pounds of food a week")
+    print(f"the{name} eats {(pound_food * 7):.2f} pounds of food a week")
 
+def getName():
+    name=input("enter your name: ")
+    return name + "*******"
+
+def displayName(first):
+    lastName=input("enter your last name: ")
+    fullName=first+""+lastName
+    return fullName
 
 # create a main function - all logic goes here
 def main():
@@ -14,6 +22,15 @@ def main():
     print()
     # call
     my_animal("lion","rawwwer",20.5)
+    print()
+    my_animal("mouse","ekekekekek",0.2)
+
+#call the  value return function
+myName=getName()
+print(myName)
+print()
+print(displayName(myName))
+
 # call the main function
 if __name__=="__name__":
     main()
